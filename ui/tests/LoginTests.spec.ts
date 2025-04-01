be3539a68b72e6c683e8eaaa20856f77f0e2f549
Pavlo <40293320+LaPavlo@users.noreply.github.com>
2025-04-01 20:49:45 +0300

    Update LoginTests.spec.ts

--- ui/tests/LoginTests.spec.ts
+++ ui/tests/LoginTests.spec.ts
@@ -4,12 +4,12 @@ import {invalidCredUser, userToLogin} from "../models/User";
 
 test('2. Login User with correct email and password', async ({ loginPage, header }) => {
     await loginPage.login(userToLogin);
-    await expect(header.logoutButton).toBeNull();
+    await expect(header.logoutButton).toBeVisible();
 })
 
 test('3. Login as User with incorrect password', async ({ loginPage }) => {
     await loginPage.login(invalidCredUser);
-    await expect(loginPage.incorrectLoginWarning).not.toBeVisible();
+    await expect(loginPage.incorrectLoginWarning).toBeVisible();
 })
 
 test('4. Log out', async ({ loginPage }) => {
